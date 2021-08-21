7b8c55c234da946faa9cc584b92e3a4f5173327a
AriGute <arielguata@gmail.com>
2021-08-21 17:48:48 +0300

    Init

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from './models/Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'shogi bombs really tasty',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'shogi bombs really not tasty',
+    day: 'May 2th at 1:50pm',
+    reminder: false,
+  },
+];
